9799bec8beab1dfc1bc44c76d039f57fc1c695c3
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-06-19 14:40:48 +0930

    Updated header file

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 16:35:57 by btomlins          #+#    #+#             */
-/*   Updated: 2023/06/16 16:35:59 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/06/19 14:08:26 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <signal.h>
 #include <unistd.h>
+#include <stdio.h>
 #include "ft_printf/ft_printf.h"
 #include "libft/libft.h"
 
