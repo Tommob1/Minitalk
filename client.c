6be680d54d8890ac2426af7532a74215856fd249
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-06-19 13:26:49 +0930

    Added changes to client.c

--- client.c
+++ client.c
@@ -6,8 +6,22 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 15:57:03 by btomlins          #+#    #+#             */
-/*   Updated: 2023/06/16 15:57:17 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/06/19 13:25:40 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
 
+void    print_bits(unsigned char octet)
+{
+    int i;
+    unsigned char   bit;
+
+    i = 8;
+
+    while (i--)
+    {
+        bit = (octet >> i & 1) + '0';
+        write(1, &bit, 1);
+    }
+}
