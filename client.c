c25cb975eb65b418100b6ddc7d20d896dfdf5e9b
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-06-19 14:40:22 +0930

    Added char_to_binary function

--- client.c
+++ client.c
@@ -6,11 +6,24 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 15:57:03 by btomlins          #+#    #+#             */
-/*   Updated: 2023/06/19 13:25:40 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/06/19 14:38:23 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void    char_to_binary(char c)
+{
+    unsigned char   mask;
+    mask = 1 << (sizeof(char) * 8 - 1);
+
+    while (mask)
+    {
+        unsigned char   bit = (c & mask) ? '1' : '0';
+        putchar(bit);
+        mask >>= 1;
+    }
+}
 
 void    print_bits(unsigned char octet)
 {
@@ -24,4 +37,9 @@ void    print_bits(unsigned char octet)
         bit = (octet >> i & 1) + '0';
         write(1, &bit, 1);
     }
+}
+
+int main(void)
+{
+    return (0);
 }
