e5256ed35eb4655b8aaf3790d9b83a4d24f6930d
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-06-21 15:12:21 +0930

    Fixed Norminette

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 15:57:03 by btomlins          #+#    #+#             */
-/*   Updated: 2023/06/21 14:52:46 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/06/21 15:10:50 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ void	char_to_binary(char c)
 	mask = 1 << (sizeof(char) * 8 - 1);
 	while (mask)
 	{
-		bit	= (c & mask) ? '1' : '0';
+		if ((c & mask) != 0)
+			bit = '1';
+		else
+			bit = '0';
 		write(1, &bit, 1);
 		mask >>= 1;
 	}
@@ -34,14 +37,14 @@ char	binary_to_char(const char	*binary)
 	{
 		return (0);
 	}
-	character = (binary[0] - '0') << 7 |
-		(binary[1] - '0') << 6 |
-		(binary[2] - '0') << 5 |
-		(binary[3] - '0') << 4 |
-		(binary[4] - '0') << 3 |
-		(binary[5] - '0') << 2 |
-		(binary[6] - '0') << 1 |
-		(binary[7] - '0');
+	character = (binary[0] - '0') << 7
+		| (binary[1] - '0') << 6
+		| (binary[2] - '0') << 5
+		| (binary[3] - '0') << 4
+		| (binary[4] - '0') << 3
+		| (binary[5] - '0') << 2
+		| (binary[6] - '0') << 1
+		| (binary[7] - '0');
 	return (character);
 }
 
