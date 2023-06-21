e7f0b6d9446016ea8352a20edc9de1b105c31424
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-06-21 14:53:55 +0930

    Fixed Norminette errors in header file

--- minitalk.h
+++ minitalk.h
@@ -6,17 +6,17 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 16:35:57 by btomlins          #+#    #+#             */
-/*   Updated: 2023/06/19 14:08:26 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/06/21 14:21:37 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <signal.h>
-#include <unistd.h>
-#include <stdio.h>
-#include "ft_printf/ft_printf.h"
-#include "libft/libft.h"
+# include <signal.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
 
 #endif
