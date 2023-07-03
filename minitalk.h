27c15ca92ea44c008e71c2a1d62ba5a6c08d3173
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-07-03 17:16:49 +0930

    Modified minitalk.h

--- minitalk.h
+++ minitalk.h
@@ -23,6 +23,6 @@
 static char bit_char(int byte[]);
 int         end(int byte[], char **string, int *pos, int *index);
 char        *reallocate(char **str, long *size, unsigned int pos);
-static void bit(int signals, siginfo_t  *info);
+static void bit(int signals, siginfo_t  *info, void *content);
 
 #endif
