ed4fe1486ccf0ec9745b297ff3918056d04aa151
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-07-05 14:57:14 +0930

    Add files via upload
    Added changes to client.c

--- client.c
+++ client.c
@@ -12,3 +12,15 @@
 
 #include "minitalk.h"
 
+static void	recieved(int signal)
+{
+	(void)signal;
+	write(1, "MESSAGE RECIEVED\n", 17);
+	exit(0);
+}
+
+int	main(void)
+{
+	recieved(20);
+	return (0);
+}
