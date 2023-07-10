e649291456f9eec46da8048988d8b28dff4b26da
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-07-10 16:20:40 +0930

    Modified client.c

--- client.c
+++ client.c
@@ -12,6 +12,31 @@
 
 #include "minitalk.h"
 
+void	bit_signal(int pid, char c, int stop)
+{
+	int	shift;
+
+	shift = 7;
+	while (stop == 1 && shift >= 0)
+	{
+		if (((c >> shift) & 1))
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(50);
+		shift--;			
+	}
+	if (stop == 0)
+	{
+		while (stop < 8)
+		{
+			kill(pid, SIGUSR1);
+			usleep(50);
+			stop++;
+		}
+	}
+}
+
 static void	recieved(int signal)
 {
 	(void)signal;
@@ -21,6 +46,9 @@ static void	recieved(int signal)
 
 int	main(void)
 {
+	int		i;
+	pid_t	pid;
+
 	recieved(20);
 	return (0);
 }
