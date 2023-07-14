248ad3b21b0fe9443c0e14c82525cf32b95ad709
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-07-14 14:45:26 +0930

    Added corrections to libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -24,18 +24,18 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-int     ft_atoi(const char *str);
-char		*ft_itoa(int n);
-void    ft_putchar_fd(char c, int fd);
-void    ft_putnbr_fd(int n, int fd);
-void    ft_putstr_fd(char *s, int fd);
-char    *ft_strdup(const char *s1);
-size_t  ft_strlen(const char *str);
-char    *ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 int		ft_printf(const char *format, ...);
 int		printf_char(int c);
 int		printf_ptint(int nb);
-int	printf_ptstr(char *str);
+int		printf_ptstr(char *str);
 
 #endif
